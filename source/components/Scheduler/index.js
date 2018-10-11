4ee3a01f3c517bf7d6f406c3ab9c0220cda2cb03
Vasilii <nitraatss@gmail.com>
2018-10-11 15:14:14 +0300

    Components html

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,15 +1,44 @@
 // Core
 import React, { Component } from 'react';
 
+// Components
+import Task from 'components/Task'
+
 // Instruments
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
+
+const tasksList = [
+
+];
 
 export default class Scheduler extends Component {
     render () {
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+              <main>
+                <header>
+                  <h1>Планировщик задач</h1>
+                  <input type="search" placholder="Посик" />
+                </header>
+                <section>
+                  <form>
+                    <input type="text" maxLength="50" placholder="Описaние моей новой задачи" />
+                    <button type="button">Добавить задачу</button>
+                  </form>
+                  <div>
+                    <ul>
+                        <Task />
+                    </ul>
+                  </div>
+                </section>
+                <footer>
+                  <div>
+                    SVG
+                  </div>
+                  <span className = {Styles.completeAllTasks}>Все задачи выполнены</span>
+                </footer>
+              </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -3,6 +3,10 @@ import React, { PureComponent } from 'react';
 
 // Instruments
 import Styles from './styles.m.css';
+import Star from 'theme/assets/Star';
+import Remove from 'theme/assets/Remove';
+import Edit from 'theme/assets/Edit';
+import Checkbox from 'theme/assets/Checkbox';
 
 export default class Task extends PureComponent {
     _getTaskShape = ({
@@ -18,6 +22,26 @@ export default class Task extends PureComponent {
     });
 
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        return (
+          <li className = { Styles.task }>
+            <div className = { Styles.content }>
+              <div className = { Styles.toggleTaskCompletedState }>
+                <Checkbox />
+              </div>
+              <input type="text" maxlength="50" value="loled" disabled />
+            </div>
+            <div className = { Styles.actions }>
+              <div className = { Styles.toggleTaskFavoriteState }>
+                <Star />
+              </div>
+              <div className = { Styles.updateTaskMessageOnClick }>
+                <Edit />
+              </div>
+              <div>
+                <Remove />
+              </div>
+            </div>
+          </li>
+        );
     }
 }
